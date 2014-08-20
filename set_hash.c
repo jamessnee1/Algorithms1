8a3a46f1e3028ae2ef098391913fbdcdd87ffccb
James Snee <jamessnee1@gmail.com>
2014-08-20 17:06:46 +1000

    hash table create and destroy

--- set_hash.c
+++ set_hash.c
@@ -9,21 +9,66 @@ set ADT for hash tables with separate chaining
 
 /******************************/
 
+/*create struct for linked list separate chaining*/
+struct list_t {
+    
+    int data;
+    struct list_t *next;
+
+};
+
 /* typedef struct set set_t is defined in set.h */
 struct set {
-	/* TODO: define for hash tables with separate chaining */
+    
+    int size;
+    struct list_t **table;
+    
 };
 
 set_t *set_create(void)
 {
-	/* TODO: define for hash tables with separate chaining */
-	return NULL;
+    int size = 10;
+    int i = 0;
+	set_t *new_set = safe_malloc(sizeof(set_t));
+    /*We need to specify a size for hash table, todo*/
+    new_set->table = safe_malloc(sizeof(struct list_t) * size);
+    
+    /*initialise elements of table*/
+    for(i = 0; i < size; i++){
+        
+        new_set->table[i] = NULL;
+    }
+    
+    new_set->size = size;
+	return new_set;
 }
 
 void set_destroy(set_t *set)
 {
-	/* TODO: define for hash tables with separate chaining */
-    set = NULL;
+	int i;
+    struct list_t *list, *temp;
+    
+    /*if nothing in hash table*/
+    
+    if (set == NULL){
+        
+        return;
+    }
+    /*free memory for everything in the hash table*/
+    for (i = 0; i < set->size; i++){
+        list = set->table[i];
+        
+        while(list != NULL){
+            temp = list;
+            list = list->next;
+            free(temp);
+        }
+    
+    }
+    
+    /*lastly, free set*/
+    free(set->table);
+    free(set);
 	return;	
 }
 
