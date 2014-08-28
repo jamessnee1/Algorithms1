91236ed92f1746aae181dc9875ccb0b5d1e73ebb
James Snee <jamessnee1@gmail.com>
2014-08-28 19:06:38 +1000

    delete working in btree

--- set_btree.c
+++ set_btree.c
@@ -1,25 +1,22 @@
 /*
-set ADT for binary trees
-*/
+ set ADT for binary trees
+ */
 
 #include "set.h"
 #include "util.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-/******************************/
+
 
 struct bst_node {
-
+    
 	int data;
 	struct bst_node *left;
 	struct bst_node *right;
 	
-
+    
 };
-
-/*function prototype for print*/
-void print(struct bst_node *node);
 
 /* typedef struct set set_t is defined in set.h */
 struct set {
@@ -27,6 +24,13 @@ struct set {
     struct bst_node *parent;
     
 };
+
+/*function prototypes*/
+void print(struct bst_node *node);
+struct bst_node * insert(int new_val);
+void insert_into_tree(struct bst_node *root, struct bst_node *new);
+void remove_from_tree(struct bst_node *root, struct bst_node *parent);
+
 
 set_t *set_create(void)
 {
@@ -51,269 +55,237 @@ void set_destroy(set_t *set)
 {
     node_destroy(set->parent);
 	free(set);
-	return;	
+	return;
 }
 
 int set_insert(set_t *set, int new_val)
 {
-
-	/* create new temp pointer to set */
-    struct bst_node **temp, **previous;
-    temp = &set->parent;
+    /*create pointer to set*/
+    struct bst_node **node, *temp;
+    node = &set->parent;
     
-	
-    /*if parent is null, insert there*/
-	if ((*temp) == NULL){
+    /*Case 1: Parent node*/
+    if ((*node) == NULL){
+        
         struct bst_node *new = safe_malloc(sizeof(struct bst_node));
         new->data = new_val;
         new->left = NULL;
         new->right = NULL;
-        previous = temp;
-		(*temp) = new;
-        printf("%i inserted into parent\n", (*temp)->data);
+		(*node) = new;
+        printf("%i inserted into parent\n", (*node)->data);
 		return 1;
-	}
-    else if ((*temp)->data == new_val){
-        /*check for root duplicate found*/
+    
+    }
+
+    /*check for duplicates*/
+    if ((*node)->data == new_val){
         return 0;
     }
-    /*if data at current node is smaller than input data, insert right*/
-    else if ((*temp)->data < new_val){
+        
+    /*point temp to a new node*/
+    temp = insert(new_val);
+        
+    /*insert function handles left and right*/
+    insert_into_tree((*node),temp);
+
+	return 1;
+}
+
+void insert_into_tree(struct bst_node *root, struct bst_node *new){
+    
+    /*if our data is smaller than the roots data, go left*/
+    if (new->data < root->data){
+        if (root->left == NULL){
+            root->left = new;
+            printf("Root of %i is %i.\n",root->left->data, root->data);
+            printf("%i inserted into left.\n", new->data);
+        }
+        else {
+            insert_into_tree(root->left, new);
+        }
+    }
+    
+    /*if our data is larger than the roots data, go right*/
+    if (new->data > root->data){
+        if (root->right == NULL){
+            root->right = new;
+            printf("Root of %i is %i.\n",root->right->data, root->data);
+            printf("%i inserted into right.\n", new->data);
+        }
+        else {
+            insert_into_tree(root->right, new);
+        }
+    }
+
+}
+
+struct bst_node * insert(int new_val){
+    
+    /*create new node*/
+    struct bst_node *new = safe_malloc(sizeof(struct bst_node));
+    new->data = new_val;
+    new->left = NULL;
+    new->right = NULL;
+    return new;
+
+}
+
+void remove_from_tree(struct bst_node *root, struct bst_node *parent){
+    
+    struct bst_node *temp, *previous;
+    
+    /*Case 1: Root has no children*/
+    if (root->left == NULL && root->right == NULL){
+        
+        /*set the previous next to null*/
+        if (parent->left == root){
             
-            while ((*temp)->right != NULL && (*temp)->data < new_val){
-                previous = temp;
-                temp = &(*temp)->right;
-                
-                if (new_val == (*temp)->data){
-                    /*duplicate found*/
-                    return 0;
-                }
-            }
-        
-            struct bst_node *new = safe_malloc(sizeof(struct bst_node));
-            new->data = new_val;
-            new->left = NULL;
-            new->right = NULL;
-            /*update previous correctly*/
-            previous = temp;
-            (*temp)->right = new;
-            printf("%i inserted into right. Previous is %i\n", (*temp)->right->data, (*previous)->data);
-            return 1;
+            parent->left = NULL;
+            
         }
-    
-    else{
-            /*data at current node is larger than input data, insert left*/
-            while ((*temp)->left != NULL && (*temp)->data > new_val){
-                previous = temp;
-                temp = &(*temp)->left;
-                
-                if ((*temp)->data == new_val){
-                    /*duplicate found*/
-                    return 0;
-                }
-            }
+        else {
+            
+            parent->right = NULL;
+            
+        }
+
+        printf("Deleted node in case 1: no left or right children.\n");
+        free(root);
+        return;
+    }
+    /*Case 2: Root has one child, left*/
+    if (root->left != NULL && root->right == NULL){
         
-            struct bst_node *new = safe_malloc(sizeof(struct bst_node));
-            new->data = new_val;
-            new->left = NULL;
-            new->right = NULL;
-            /*update previous correctly*/
+        /*special case for root of tree*/
+        if (parent == root){
+            /*find the largest element of left subtree to replace*/
+            temp = root->left;
+            
+            /*traverse left until we can't go any further*/
+            while (temp->right != NULL){
+                temp = temp->right;
+            }
+            
+            if (temp->right == NULL){
+                /*set the original node's data to smallest in right subtree*/
+                root->data = temp->data;
+                printf("Special case parent - left not null\n");
+                free(temp);
+            }
+            return;
+        }
+        
+        
+        /*set temp to root node to delete*/
+        temp = root;
+        parent->left = temp->left;
+        printf("Case 2: one child left, deleted %i\n", temp->data);
+        free(temp);
+        return;
+    }
+    /*Case 2: Root has one child, right*/
+    if (root->left == NULL && root->right != NULL){
+        
+        /*special case for root of tree*/
+        if (parent == root){
+            /*find the smallest element of right subtree to replace*/
+            temp = root->right;
+            
+            /*traverse left until we can't go any further*/
+            while (temp->left != NULL){
+                temp = temp->left;
+            }
+            
+            if (temp->left == NULL){
+                /*set the root node's data to smallest in right subtree*/
+                root->data = temp->data;
+                printf("Special case parent - right not null\n");
+                free(temp);
+            }
+            return;
+        }
+        
+        /*set temp to root node to delete*/
+        temp = root;
+        parent->right = temp->right;
+        printf("Case 2: one child right, deleted %i\n", temp->data);
+        free(temp);
+        return;
+        
+    }
+    /*Case 3: Root has two children*/
+    if (root->left != NULL && root->right != NULL){
+
+        /*go right once then left to find the smallest value to replace node value*/
+        temp = root->right;
+        /*traverse left until we can't go any further*/
+        while (temp->left != NULL){
             previous = temp;
-            (*temp)->left = new;
-            printf("%i inserted into left. Previous is %i\n", (*temp)->left->data, (*previous)->data);
-            return 1;
+            temp = temp->left;
+        }
+        
+        if (temp->left == NULL){
+            /*change the value of root data to bottom of BST*/
+            root->data = temp->data;
+            previous->left = NULL;
+            printf("Case 3: Root has two children. Root node changed to %i\n", root->data);
+            printf("temp is %i\n", temp->data);
+            free(temp);
+            return;
         
         }
-    
-	return 0;
+
+        
+        return;
+    }
+
 }
 
 int set_delete(set_t *set, int del_val)
 {
-	
-    struct bst_node **node = NULL, **previous = NULL, *temp;
-    node = &set->parent;
+    struct bst_node *temp, *previous;
+    temp = set->parent;
     int found = 0;
     
     /*nothing in tree*/
     if (set->parent == NULL){
         return 0;
-   
+        
     }
     
-        while ((*node) != NULL){
+    while (temp != NULL){
+        
+        /* if our data is at node, break */
+        if (temp->data == del_val){
+            found = 1;
+            break;
+        }
+        else {
             
-            previous = node;
-            
-            /* if our data is at node, break */
-            if ((*node)->data == del_val){
-                found = 1;
-                break;
+            /*left traversal*/
+            if (temp->data > del_val){
+                previous = temp;
+                temp = temp->left;
+                
+                
             }
+            /*right traversal*/
             else {
+                previous = temp;
+                temp = temp->right;
                 
-                /*left traversal*/
-                if ((*node)->data > del_val){
-                    previous = node;
-                    node = &(*node)->left;
-                    
-                }
-                /*right traversal*/
-                else {
-                    previous = node;
-                    node = &(*node)->right;
-                    
-                }
             }
-            
         }
- 
-        if (found == 1){
-            /*found it*/
-            /*CASE 1: No left or right children*/
-            if ((*node)->left == NULL && (*node)->right == NULL){
-
-                if ((*previous)->left == *node){
-                    
-                    (*previous)->left = NULL;
-                    
-                }
-                else {
-                
-                    (*previous)->right = NULL;
-                    
-                }
-                
-                printf("Deleted node in case 1: no left or right children. prev is %i\n", (*previous)->data);
-                free(*node);
-                return 1;
-
-            }
-            
-            /*CASE 2: node has one child - left*/
-            if ((*node)->left != NULL && (*node)->right == NULL){
-            
-                temp = (*node)->left;
-                
-                /*parent special case*/
-                if ((*previous) == *node){
-                    printf("Parent special case - left node not null. prev is %i\n", (*previous)->data);
-                    (*node)->data = temp->data;
-                    /*temp left needs to link back to previous left*/
-                    if ((*node)->left != NULL){
-                        (*node)->left = temp->left;
-                    }
-                    printf("Deleted temp - %i\n", temp->data);
-                    temp = NULL;
-                    free(temp);
-                    return 1;
-                }
-                
-                /*if previous left is our data*/
-                if ((*previous)->left == *node){
-                    
-                    /*point the next data to prev left*/
-                    (*previous)->left = (*node)->left;
-                }
-                else {
-                
-                    (*previous)->right = (*node)->right;
-                }
-                printf("Deleted node in case 2: one child-left. prev is %i\n", (*previous)->data);
-                free(*node);
-                return 1;
-                
-            }
-            
-            /*CASE 2: node has one child - right*/
-            if ((*node)->right != NULL && (*node)->left == NULL){
-                
-                temp = (*node)->right;
-                
-                /*parent special case*/
-                if ((*previous) == *node){
-                    printf("Parent special case - right node not null. prev is %i\n", (*previous)->data);
-                    (*node)->data = temp->data;
-                    /*temp left needs to link back to previous left*/
-                    if ((*node)->right != NULL){
-                        (*node)->right = temp->right;
-                    }
-                    printf("Deleted temp - %i\n", temp->data);
-                    temp = NULL;
-                    free(temp);
-                    return 1;
-                    
-                }
-                
-                /*if previous left is our data*/
-                if ((*previous)->left == *node){
-                    
-                    (*previous)->left = (*node)->left;
-                }
-                else {
-                    
-                    (*previous)->right = (*node)->right;
-                }
-                printf("Deleted node in case 2: one child-right. prev is %i\n", (*previous)->data);
-                free(*node);
-                return 1;
-                
-            }
-            
-            /*CASE 3: two children*/
-            if ((*node)->right != NULL && (*node)->left != NULL){
-                
-                temp = (*node)->right;
-                /*if bottom of bst has no children*/
-                if (temp->right == NULL && temp->left == NULL){
-                
-                    /*set the root data to the rights data*/
-                    (*node)->data = temp->data;
-                    (*node)->right = NULL;
-                    printf("Deleted node in case 3: both null. prev is %i\n", (*previous)->data);
-                    free(temp);
-                    return 1;
-                    
-                }
-                /*if temp has left child*/
-                else if (temp->left != NULL && temp->right == NULL) {
-                    
-                    /*traverse left*/
-                    while(temp->left != NULL){
-                    
-                        (*previous) = temp;
-                        temp = temp->left;
-                        
-
-                    }
-                    /*set the root data to the left child data*/
-                    (*node)->data = temp->data;
-                    (*previous)->left = NULL;
-                    printf("Deleted node in case 3: left not null, right null. prev is %i\n", (*previous)->data);
-                    free(temp);
-                    return 1;
-                
-                
-                }
-                /*if temp has right child*/
-                else if (temp->right != NULL && temp->left == NULL){
-                    
-                    /*set the root data to the right child data*/
-                    (*node)->data = temp->data;
-                    /*set our root next right to temp right*/
-                    (*node)->right = temp->right;
-                    printf("Deleted node in case 3: right not null, left null. prev is %i\n", (*previous)->data);
-                    free(temp);
-                    return 1;
-                
-                }
-            
-            }
-            
-        }
+        
+    }
     
-
-	return 0;	
+    if (found == 1){
+        /*found it*/
+        remove_from_tree(temp, previous);
+        return 1;
+  
+    }
+	return 0;
 }
 
 int set_search(set_t *set, int search_val)
@@ -323,7 +295,7 @@ int set_search(set_t *set, int search_val)
     temp = set->parent;
     
     while (temp != NULL){
-    
+        
         /* if our data is at node, return */
         if (temp->data == search_val){
             return 1;
@@ -338,10 +310,10 @@ int set_search(set_t *set, int search_val)
                 temp = temp->right;
             }
         }
-    
+        
     }
     
-	return 0;	
+	return 0;
 }
 
 void set_print(set_t *set)
@@ -351,15 +323,13 @@ void set_print(set_t *set)
 }
 
 void print(struct bst_node *node){
-
+    /*prints tree in preorder*/
     if (node){
-        print(node->left);
         printf("%d\n", node->data);
+        print(node->left);
         print(node->right);
-    
+        
+        
     }
-
+    
 }
-
-
-
